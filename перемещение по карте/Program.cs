a2abae49c5719783167873b9878a4d95ed329962
Мария Журинова <Zhurinova_2003@mail.ru>
2022-04-05 18:06:51 +0300

    Добавьте файлы проекта.

---
+++ перемещение по карте/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.Write("* * * * * * * * * *\n" +
+              "*                 *\n" +
+              "*                 *\n" +
+              "*                 *\n" +
+              "*                 *\n" +
+              "*                 *\n" +
+              "*                 *\n" +
+              "*                 *\n" +
+              "*                 *\n" +
+              "*                 *\n" +
+              "* * * * * * * * * *");
+int x = 0, y = 0;
+int shiftX = 9, shiftY = 5;
+Console.SetCursorPosition(x + shiftX, y + shiftY);
+Console.Write("@");
+
+while (true)
+{
+    ConsoleKeyInfo move = Console.ReadKey(true);
+    Console.SetCursorPosition(x + shiftX, y + shiftY);
+    Console.Write(" ");
+    {
+        switch (move.Key)
+        {
+            case ConsoleKey.W:
+                y--;
+
+                break;
+            case ConsoleKey.A:
+                x--;
+                break;
+            case ConsoleKey.S:
+                y++;
+                break;
+            case ConsoleKey.D:
+                x++;
+                break;
+        }
+        Console.SetCursorPosition(x + shiftX, y + shiftY);
+        Console.Write("@");
+        Console.SetCursorPosition(15, 15);
+    }
+}
